098bab17030bfc4b1be9355bb11da87885b1be0b
BaldFabi <thebaldfabi@gmail.com>
2024-09-03 09:22:31 +0200

    refactor: introduce new keymaps

--- lua/BaldFabi/remap.lua
+++ lua/BaldFabi/remap.lua
@@ -1,7 +1,7 @@
 vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
 
-vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
-vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
 vim.keymap.set('n', '<leader>s#', '<CMD>vsplit<CR>', { noremap = false })
 vim.keymap.set('n', '<leader>s-', '<CMD>split<CR>', { noremap = false })
