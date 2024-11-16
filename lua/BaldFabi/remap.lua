b57059fbda87fb3cad00a74b32034e2f7ab9ab9c
BaldFabi <thebaldfabi@gmail.com>
2024-11-16 23:46:08 +0100

    feat: add rename for current buffer keymap

--- lua/BaldFabi/remap.lua
+++ lua/BaldFabi/remap.lua
@@ -1,7 +1,7 @@
 vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
 
-vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
-vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", { noremap = false })
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", { noremap = false })
 
 vim.keymap.set('n', '<leader>s#', '<CMD>vsplit<CR>', { noremap = false })
 vim.keymap.set('n', '<leader>s-', '<CMD>split<CR>', { noremap = false })
@@ -12,11 +12,12 @@ vim.keymap.set('n', '-', '<CMD>vertical resize -10<CR>', { noremap = false })
 
 vim.keymap.set('i', 'jk', '<ESC>', { noremap = false })
 
-vim.keymap.set('n', 'ö', ']m', { noremap = false })
 vim.keymap.set('n', 'Ö', '[m', { noremap = false })
+vim.keymap.set('n', 'ö', ']m', { noremap = false })
 
 vim.keymap.set('n', '<C-n>', '<CMD>bnext<CR>', { noremap = false })
 vim.keymap.set('n', '<C-p>', '<CMD>bprevious<CR>', { noremap = false })
 vim.keymap.set('n', '<leader>bx', '<CMD>bd<CR>', { noremap = false })
 
 vim.keymap.set('n', '<leader>rr', '<CMD>LspRestart<CR>', { noremap = false })
+vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]], { noremap = false })
