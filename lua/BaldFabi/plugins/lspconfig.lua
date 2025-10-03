f69c63371a6d0df591920f562dfef94d8fa926b3
BaldFabi <thebaldfabi@gmail.com>
2025-10-03 21:27:30 +0200

    fix: mason repo references

--- lua/BaldFabi/plugins/lspconfig.lua
+++ lua/BaldFabi/plugins/lspconfig.lua
@@ -1,101 +1,107 @@
 return {
-  'neovim/nvim-lspconfig',
-  dependencies = {
-    "williamboman/mason.nvim",
-    "williamboman/mason-lspconfig.nvim",
-    "nvimtools/none-ls.nvim",
+	"neovim/nvim-lspconfig",
+	dependencies = {
+		"mason-org/mason.nvim",
+		"mason-org/mason-lspconfig.nvim",
+		"nvimtools/none-ls.nvim",
 
-    "j-hui/fidget.nvim",
+		"j-hui/fidget.nvim",
 
-    "folke/neodev.nvim",
-  },
-  config = function()
-    local servers = {
-      vue_ls = {},
-      html = {},
-      ansiblels = {},
-      gopls = {},
-      pyright = {},
-      rust_analyzer = {},
-      ts_ls = {},
-      marksman = {},
-      terraformls = {},
-      tailwindcss = {
-        filetypes = {
-          'templ',
-          'html'
-        },
-        init_options = {
-          userLanguages = {
-            templ = "html"
-          }
-        }
-      },
-      yamlls = {},
-      lua_ls = {
-        Lua = {
-          workspace = { checkThirdParty = false },
-          telemetry = { enable = false },
-        },
-      },
-      templ = {},
-    }
+		"folke/neodev.nvim",
+	},
+	config = function()
+		local servers = {
+			vue_ls = {},
+			html = {},
+			ansiblels = {},
+			gopls = {},
+			pyright = {},
+			rust_analyzer = {},
+			ts_ls = {},
+			marksman = {},
+			terraformls = {},
+			tailwindcss = {
+				filetypes = {
+					"templ",
+					"html",
+				},
+				init_options = {
+					userLanguages = {
+						templ = "html",
+					},
+				},
+			},
+			yamlls = {},
+			lua_ls = {
+				Lua = {
+					workspace = { checkThirdParty = false },
+					telemetry = { enable = false },
+				},
+			},
+			templ = {},
+		}
 
-    require("neodev").setup()
+		require("neodev").setup()
 
-    local capabilities = vim.lsp.protocol.make_client_capabilities()
-    capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
+		local capabilities = vim.lsp.protocol.make_client_capabilities()
+		capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities)
 
-    require("mason").setup()
+		require("mason").setup()
 
-    local mason_lspconfig = require("mason-lspconfig")
+		local mason_lspconfig = require("mason-lspconfig")
 
-    mason_lspconfig.setup {
-      ensure_installed = vim.tbl_keys(servers),
-    }
+		mason_lspconfig.setup({
+			ensure_installed = vim.tbl_keys(servers),
+		})
 
-    local handlers = {
-      ["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, { border = "single" }),
-      ["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, { border = "single" }),
-    }
+		local handlers = {
+			["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, { border = "single" }),
+			["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, { border = "single" }),
+		}
 
-    local on_attach = function(_, bufnr)
-      vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, { noremap = false })
+		local on_attach = function(_, bufnr)
+			vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, { noremap = false })
 
-      vim.keymap.set('n', 'gd', vim.lsp.buf.definition, { noremap = false })
-      vim.keymap.set('n', 'gr', require('telescope.builtin').lsp_references, { noremap = false })
-      vim.keymap.set('n', 'gI', vim.lsp.buf.implementation, { noremap = false })
-      vim.keymap.set('n', '<leader>D', vim.lsp.buf.type_definition, { noremap = false })
-      vim.keymap.set('n', '<leader>ds', require('telescope.builtin').lsp_document_symbols, { noremap = false })
-      vim.keymap.set('n', '<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, { noremap = false })
+			vim.keymap.set("n", "gd", vim.lsp.buf.definition, { noremap = false })
+			vim.keymap.set("n", "gr", require("telescope.builtin").lsp_references, { noremap = false })
+			vim.keymap.set("n", "gI", vim.lsp.buf.implementation, { noremap = false })
+			vim.keymap.set("n", "<leader>D", vim.lsp.buf.type_definition, { noremap = false })
+			vim.keymap.set("n", "<leader>ds", require("telescope.builtin").lsp_document_symbols, { noremap = false })
+			vim.keymap.set(
+				"n",
+				"<leader>ws",
+				require("telescope.builtin").lsp_dynamic_workspace_symbols,
+				{ noremap = false }
+			)
 
-      vim.keymap.set('n', 'K', vim.lsp.buf.hover, { noremap = false })
-      vim.keymap.set('n', '<C-s>', vim.lsp.buf.signature_help, { noremap = false })
+			vim.keymap.set("n", "K", vim.lsp.buf.hover, { noremap = false })
+			vim.keymap.set("n", "<C-s>", vim.lsp.buf.signature_help, { noremap = false })
 
-      vim.api.nvim_buf_create_user_command(bufnr, 'Format', function(_)
-        vim.lsp.buf.format()
-      end, { desc = 'Format current buffer with LSP' })
+			vim.api.nvim_buf_create_user_command(bufnr, "Format", function(_)
+				vim.lsp.buf.format()
+			end, { desc = "Format current buffer with LSP" })
 
-      vim.api.nvim_command(
-        'autocmd BufWritePre *.go,*.tf,*.js,*.tsx,*.ts,*.md,*.css,*.scss,*.sass,*.yaml,*.yml,*.json,*.html,*.lua,*.templ,*.vue,*.graphql :Format')
-    end
+			vim.api.nvim_command(
+				"autocmd BufWritePre *.go,*.tf,*.js,*.tsx,*.ts,*.md,*.css,*.scss,*.sass,*.yaml,*.yml,*.json,*.html,*.lua,*.templ,*.vue,*.graphql :Format"
+			)
+		end
 
-    for _, lsp in ipairs(vim.tbl_keys(servers)) do
-      vim.lsp.config(lsp, {
-        on_attach = on_attach,
-        handlers = handlers,
-        capabilities = capabilities,
-        settings = servers[lsp],
-      })
-    end
+		for _, lsp in ipairs(vim.tbl_keys(servers)) do
+			vim.lsp.config(lsp, {
+				on_attach = on_attach,
+				handlers = handlers,
+				capabilities = capabilities,
+				settings = servers[lsp],
+			})
+		end
 
-    local null_ls = require('null-ls')
-    null_ls.setup({
-      on_attach = on_attach,
-      sources = {
-        null_ls.builtins.formatting.goimports,
-        null_ls.builtins.formatting.prettier,
-      }
-    })
-  end
+		local null_ls = require("null-ls")
+		null_ls.setup({
+			on_attach = on_attach,
+			sources = {
+				null_ls.builtins.formatting.goimports,
+				null_ls.builtins.formatting.prettier,
+			},
+		})
+	end,
 }
--- lua/BaldFabi/plugins/mason-null-ls.lua
+++ lua/BaldFabi/plugins/mason-null-ls.lua
@@ -1,13 +1,13 @@
 return {
-        "jay-babu/mason-null-ls.nvim",
-        event = { "BufReadPre", "BufNewFile" },
-        dependencies = {
-                "williamboman/mason.nvim",
-                "nvimtools/none-ls.nvim",
-        },
-        config = function()
-                require("mason-null-ls").setup({
-                        ensure_installed = { "goimports", "prettier" }
-                })
-        end,
+	"jay-babu/mason-null-ls.nvim",
+	event = { "BufReadPre", "BufNewFile" },
+	dependencies = {
+		"mason-org/mason.nvim",
+		"nvimtools/none-ls.nvim",
+	},
+	config = function()
+		require("mason-null-ls").setup({
+			ensure_installed = { "goimports", "prettier" },
+		})
+	end,
 }
